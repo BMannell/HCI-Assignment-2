43dca410fd09a1914d8e4d3b8430b08530fe5655
d-johnston <djkj93@gmail.com>
2015-02-26 12:37:50 -0500

    work on deposit + keypad js cleanup

--- numeric-keypad.js
+++ numeric-keypad.js
@@ -1,30 +1,27 @@
-$(document).ready(function () {
+<script>
+	$(function(){
+		// Code for Keypad Button Password Entry
+		$('.number-button').click(function () {
+			var insertText = $('#keypad-input').val() + $(this).attr('value');
+			$('#keypad-input').val(insertText);
+		});
 
-    
-	
-	// Code for Numeric Keypad Button Password Entry
-    $('.number-button').click(function () {
-        var insertText = $('#keypad-input').val() + $(this).attr('value');
-        $('#keypad-input').val(insertText);
-    });
+		$('.clear-button').click(function () {
+			//used to be variable assignment here that went in val() below
+			$('#keypad-input').val('');
+		});
 
-    $('.clear-button').click(function () {
-        //used to be variable assignment here that went in val() below
-        $('#keypad-input').val('');
-    });
-
-
-
-    $('.del-button').click(function () {
-        //used to be variable assignment here that went in val() below
-        var str = $('#keypad-input').val();
-        str = str.substring(0,str.length-1);
-        $('#keypad-input').val(str);
-    });
+		$('.del-button').click(function () {
+			//used to be variable assignment here that went in val() below
+			var str = $('#keypad-input').val();
+			str = str.substring(0,str.length-1);
+			$('#keypad-input').val(str);
+		});
 
 
-    $('.ok-button').click(function () {
-        //call affirmative action
-        
-    });
-});
+		$('.ok-button').click(function () {
+			//call affirmative action
+
+		});
+	});
+</script>
